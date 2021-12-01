a806a91991c6f9764ae1ba95486fead7ae06f3d0
carlos114771 <alfreoldoriverac25@gmail.com>
2021-11-30 21:38:07 -0600

    Add files via upload

---
+++ agglomerative_clustering_ward.py
@@ -0,0 +1,41 @@
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+def agglomerative_clustering(data, num):
+    labels = [1,2,3,4,5]
+    for x in range(1, 6):
+        path = './plots/agglomerative clustering (ward)/agglomerative'
+        a = AgglomerativeClustering(linkage = 'ward', n_clusters = x).fit(data)
+        scatter = plt.scatter(data[:,0], data[:,1], c=a.labels_, cmap='Accent')
+        plt.legend(handles=scatter.legend_elements()[0], labels = labels)
+        plt.title("agglomerative clustering with Ward Linkage")
+        path += '_cluster_' + str(x) + '_dataset_' + str(num) + '.png'
+        plt.savefig(path)
+
+
+def agglomerative_clustering_distance(data, num):
+    labels = [1,2,3,4,5]
+    distance = [0.25, 0.50, 0.75, 1.0, 1.5]
+    for x in distance:
+        path = './plots/agglomerative clustering (ward)/agglomerative'
+        a = AgglomerativeClustering(linkage = 'ward', n_clusters = None, distance_threshold=x).fit(data)
+        scatter = plt.scatter(data[:,0], data[:,0], c=a.labels_, cmap='Accent')
+        plt.legend(handles=scatter.legend_elements()[0], labels = labels)
+        plt.title("agglomerative clustering ward")
+        path += '_distance_' + str(x) + '_dataset_' + str(num) + '.png'
+        plt.savefig(path)
+
+
+def main(path, n):
+    data = np.genfromtxt(path, delimiter=',', skip_header=1)
+    agglomerative_clustering(data, n)
+    agglomerative_clustering_distance(data, n)
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    n = sys.argv[-1]
+    main(path, n)
---
+++ k-means.py
@@ -0,0 +1,27 @@
+from sklearn.cluster import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+def K_means(data, num):
+    labels = [1,2,3,4,5]
+    for x in range(1, 6):
+        path = './plots/k-means/k-means'
+        kmeans = KMeans(n_clusters=x).fit(data)
+        scatter = plt.scatter(data[:,0], data[:,1], c=kmeans.labels_, cmap='Accent')
+        plt.legend(handles=scatter.legend_elements()[0], labels = labels)
+        plt.title("k-means")
+        path += '_' + str(x) + '_datos_' + str(num) + '.png'
+        plt.savefig(path)
+
+
+def main(path, n):
+    data = np.genfromtxt(path, delimiter=',', skip_header=1)
+    K_means(data, n)
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    n = sys.argv[-1]
+    main(path, n)
